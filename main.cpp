0a5269deb6344be87c5ed9a510cd13ef7ef33c62
Evelyn Colon <139907540+EvieColon@users.noreply.github.com>
2024-04-19 18:39:34 -0400

    Update main.cpp
    Added code used to test schedule creation functionality

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include "json.h"
 #include "curlUtils.h"
 #include <utility>
+#include <tuple>
+#include "ScheduleMaker.h"
 using namespace std;
 using namespace std::chrono;
 using json = nlohmann::json;
@@ -180,6 +182,42 @@ string refreshToken() {
 
 
 int main() {
+    // Evelyn Colon: test of graph code
+    vector<string> dependentTasks1 = {"Complete Review Sheet", "Complete Practice Exam", "Review MATLAB"};
+    tuple<string, int, vector<string>> task1 = make_tuple("Watch Review Lectures", 50, dependentTasks1);
+    vector<string> dependentTasks2 = {"Review Lecture Notes"};
+    tuple<string, int, vector<string>> task2 = make_tuple("Complete Review Sheet", 120, dependentTasks2);
+    tuple<string, int, vector<string>> task3 = make_tuple("Complete Practice Exam", 120, dependentTasks2);
+    tuple<string, int, vector<string>> task4 = make_tuple("Review MATLAB", 30, dependentTasks2);
+    vector<string> emptyDependency;
+    tuple<string, int, vector<string>> task5 = make_tuple("Review Lecture Notes", 60, emptyDependency);
+    vector<tuple<string, int, vector<string>>> tasks = {task1, task2, task3, task4, task5};
+    vector<tuple<string, int, int>> schedule = ScheduleMaker(tasks);
+    string prevEndTime = "9:00AM";
+    for (int i = 0; i < schedule.size(); i++) {
+        string name = get<0>(schedule[i]);
+        int minutes = get<1>(schedule[i]);
+        int breakTime = get<2>(schedule[i]);
+        string startTime = prevEndTime;
+        bool morning;
+        if (startTime[startTime.length() - 2] == 'A') {
+            morning = true;
+        }
+        else {
+            morning = false;
+        }
+        prevEndTime = getEndTime(startTime, minutes, morning);
+        cout << startTime << ": " << name << endl;
+        startTime = prevEndTime;
+        if (startTime[startTime.length() - 2] == 'A') {
+            morning = true;
+        }
+        else {
+            morning = false;
+        }
+        prevEndTime = getEndTime(startTime, breakTime, morning);
+        cout << startTime << ": " << "Break!" << endl;
+    }
     string genre = "jazz";
     string token = refreshToken();
 
@@ -216,7 +254,6 @@ int main() {
     for (const auto& song : songs) {
         cout << "Song Name: " << song.first << ", Duration: " << song.second << "ms" << endl;
     }
-
     return 0;
 }
 
