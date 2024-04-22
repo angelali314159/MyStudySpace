de5a3e3bc34bb5f854b6ef83fe2ac86fb4f4e109
Evelyn Colon <139907540+EvieColon@users.noreply.github.com>
2024-04-21 21:26:09 -0400

    Update ScheduleMaker.h
    added clear function declaration

--- ScheduleMaker.h
+++ ScheduleMaker.h
@@ -8,3 +8,4 @@ using namespace std;
 vector<tuple<string, int, int>> ScheduleMaker(vector<tuple<string, int, vector<string>>> tasks);
 int getBreak(int val);
 string getEndTime(string startTime, int minutes);
+void clear();
