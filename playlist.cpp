fad00bd7d7d8b28a63c1d773ea8bc20d0d837c84
Evelyn Colon <139907540+EvieColon@users.noreply.github.com>
2024-04-21 22:26:57 -0400

    Update playlist.cpp
    working on bin packing

--- playlist.cpp
+++ playlist.cpp
@@ -6,26 +6,32 @@
 using namespace std;
 
 // create playlist using first-fit bin-packing algorithm
-    vector<Playlist> PlaylistGenerator::createPlaylist(const vector<pair<std::string, int>> &songs, int task_duration) {
-        std::vector<Playlist> playlist;
-        int totalDuration = 0;
-        int task_duration_seconds = task_duration*60;
+    vector<Playlist> PlaylistGenerator::createPlaylists(const vector<pair<std::string, int>> &songs, vector<tuple<string, int, int>> tasks) {
+        vector<vector<Playlist>> playlists(task.size());
+        vector<int> bins;
+        for (int i = 0; i < tasks.size(); i++) {
+            bins.push_back(get<1>(tasks[i]));
+        }
 
-    for (const auto& song : songs) {
-        // Convert duration from milliseconds to seconds
-        int durationSeconds = song.second / 1000;
-
-        // Iterate over sorted songs
-    if (totalDuration + durationSeconds > task_duration_seconds) {
-        break;
+        for (const auto& song : songs) {
+            // Convert duration from milliseconds to seconds
+            int durationSeconds = song.second / 1000;
+            int gap = MAX_INTEGER;
+            int binIndex = -1;
+            for (int i = 0; i < tasks.size(); i++) {
+                if ((bins[i] - durationSeconds < gap) && (bins[i] - durationSeconds >= 0)) {
+                    gap = bins[i] - durationSeconds;
+                    binIndex = i;
+                }
+            }
+            if (binIndex == -1) {
+                break;
+            }
+            bins[binIndex] -= durationSeconds;
+            Playlist playlistItem;
+            playlistItem.songName = song.first;
+            playlistItem.duration = durationSeconds;
+            playlists[i].push_back(playlistItem);
+        }
+        return playlists;
     }
-
-    Playlist playlistItem;
-    playlistItem.songName = song.first;
-    playlistItem.duration = durationSeconds;
-    playlist.push_back(playlistItem);
-
-    totalDuration += durationSeconds;
-}
-return playlist;
-}
